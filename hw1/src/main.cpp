fc5dee3a188fc33feccbfb0b356d0d38d128fcea
poiuntwl <vovanchik999999999@gmail.com>
2026-08-13 21:33:55 +0300

    init

---
+++ hw1/src/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <Arduino.h>
+#define LED 2
+#define EXT_LED 4
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(LED, OUTPUT);
+    pinMode(EXT_LED, OUTPUT);
+}
+
+void loop() {
+    Serial.println("Hello world");
+    digitalWrite(EXT_LED, HIGH);
+    delay(100);
+    digitalWrite(EXT_LED, LOW);
+    delay(100);
+}
