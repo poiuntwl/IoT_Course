1fe88bbaafe39f24ca7c39a41792d17c9f707e48
volodya <cont_vlenio@cont.plasticsurgery.org>
2026-08-20 19:06:42 +0300

    finish

--- hw2/src/main.cpp
+++ hw2/src/main.cpp
@@ -11,22 +11,32 @@ namespace {
 
 static uint8_t random_uint8_t(uint8_t min, uint8_t max);
 
+static void log_data(SensorData sd);
+
 void setup() {
     Serial.begin(115200);
 }
 
+static ulong lastLogTime = 0;
+static ulong launchTime = millis();
+static SensorData sd;
+
 void loop() {
-    const SensorData sd = {
-        .temperature = random_uint8_t(15, 30),
-        .humidity = random_uint8_t(30, 65),
-        .timestamp = time(nullptr)
-    };
+    const unsigned long now = millis();
+    if (now - lastLogTime >= 20000) {
+        lastLogTime = now;
+        sd = {
+            .temperature = random_uint8_t(15, 30),
+            .humidity = random_uint8_t(30, 65),
+            .timestamp = time(nullptr)
+        };
 
-    char buffer[64];
-    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", localtime(&sd.timestamp));
+        log_data(sd);
 
-    Serial.printf("temperature: %uC\thumidity: %u%\tdatetime: %s\r\n", sd.temperature, sd.humidity, buffer);
-    delay(1000);
+        if (now - launchTime <= 5 * 60 * 1000) {
+            Serial.printf("%u\r\n", ESP.getFreeHeap());
+        }
+    }
 }
 
 static uint8_t random_uint8_t(const uint8_t min, const uint8_t max) {
@@ -35,4 +45,12 @@ static uint8_t random_uint8_t(const uint8_t min, const uint8_t max) {
 
     std::uniform_int_distribution<uint8_t> dist(min, max);
     return dist(gen);
+}
+
+void log_data(SensorData sd) {
+    char buffer[64];
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", localtime(&sd.timestamp));
+
+    Serial.printf("temperature: %uC\thumidity: %u%\tdatetime: %s\r\n", sd.temperature, sd.humidity, buffer);
+    delay(1000);
 }
