4d62fddbafb4f4bfa82ac2e749eabfb4753d78f5
volodya <cont_vlenio@cont.plasticsurgery.org>
2026-08-27 16:28:55 +0300

    hw3: add comments for clarity

--- hw3/src/main.cpp
+++ hw3/src/main.cpp
@@ -1,3 +1,25 @@
+/// components:
+/// button:         digital input
+/// photoresistor:  analog input
+/// DHT22:          digital protocol
+/// LED:            digital output
+///
+/// BUTTON:
+/// switches between SILENCE and MONITOR
+/// uses debounce
+/// MONITOR: read each 5s: temperature, humidity, brightness
+/// output: serial monitor (TX)
+///
+/// LED:
+/// if brightness < threshold: turn on
+///
+/// WEB:
+/// each 30s: POST httpbin.org/post (or locally set up server)
+///
+/// EDGE CASES:
+/// WI-FI unavailable: output message, continue
+/// sensor errors: output message, continue
+
 namespace {
     void setup() {
     }
