972c29cd9801bcfcf08b0a997a1a70e96b1d5984
volodya <cont_vlenio@cont.plasticsurgery.org>
2026-09-03 00:09:45 +0300

    ensure wifi, refactor

--- hw4/src/main.cpp
+++ hw4/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <DHTesp.h>
+#include <WiFi.h>
 #include <PubSubClient.h>
 
 constexpr int DHT_PIN = 26;
@@ -11,18 +12,34 @@ void setup() {
     Serial.begin(115200);
     dht.setup(DHT_PIN, DHTesp::DHT22);
     pinMode(BTN_PIN, INPUT_PULLUP);
+    WiFi.begin("Wokwi-GUEST");
 }
 
 static void pubTnH();
 
+static void ensureWifi();
+
 static ulong lastSentTs;
+static ulong lastWifiCheckTs;
 
 void loop() {
-    if (millis() - lastSentTs > 10000) {
-        pubTnH();
-    }
+    ensureWifi();
+    pubTnH();
 }
 
 
 static void pubTnH() {
+    if (millis() - lastSentTs >= 10000) {
+        Serial.println("publish TH");
+    }
+}
+
+static void ensureWifi() {
+    if (WiFi.isConnected()) {
+        return;
+    }
+    if (millis() - lastWifiCheckTs >= 5000) {
+        lastWifiCheckTs = millis();
+        WiFi.begin();
+    }
 }
