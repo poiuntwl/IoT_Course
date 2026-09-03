aa93034e4d5890432faf475ecf38cd2b41e79790
volodya <cont_vlenio@cont.plasticsurgery.org>
2026-09-03 19:21:35 +0300

    add blinking

--- hw4/esp32_receiver/src/main.cpp
+++ hw4/esp32_receiver/src/main.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 constexpr uint8_t LED_PIN = 19;
 
-constexpr char MQTT_HOST[] = "test.mosquitto.org";
-constexpr uint16_t MQTT_PORT = 1883;
+static constexpr char MQTT_HOST[] = "test.mosquitto.org";
+static constexpr uint16_t MQTT_PORT = 1883;
+static constexpr uint8_t BLINK_COUNT = 3;
+static constexpr ulong BLINK_INTERVAL_MS = 500;
 
 constexpr char SENSOR_TOPIC[] =
         "iot-course/volodymyr_1de632fd-de4a-4802-99e0-11c756d002c4/sensors";
@@ -19,26 +22,43 @@ constexpr char SENSOR_PAYLOAD_FORMAT[] =
 static WiFiClient wifiClient;
 static PubSubClient mqttClient(wifiClient);
 
+static bool blinking = false;
+static bool blinkState = false;
+static uint8_t blinkTransitions = 0;
+static ulong lastBlinkTs = 0;
+
 static void ensureWifi();
 
 static void ensureMQTT();
 
+static void execCommand(const byte *payload, unsigned int length);
+
+static void updateSensors(byte *payload, unsigned int length);
+
+void updateBlink();
+
 void setup() {
     Serial.begin(115200);
+    pinMode(LED_PIN, OUTPUT);
 
     WiFi.begin("Wokwi-GUEST");
 
     mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-    mqttClient.setCallback([](char *topic, const byte *payload, const unsigned int length) {
-        String message;
+    mqttClient.setCallback([](
+    const char *topic,
+    byte *payload,
+    const unsigned int length
+) {
+            if (strcmp(topic, COMMANDS_TOPIC) == 0) {
+                execCommand(payload, length);
+            }
 
-        for (unsigned int i = 0; i < length; ++i) {
-            message += static_cast<char>(payload[i]);
-        }
+            if (strcmp(topic, SENSOR_TOPIC) == 0) {
+                updateSensors(payload, length);
+            }
 
-        Serial.printf("Topic: %s\r\n", topic);
-        Serial.printf("Payload: %s\r\n", message.c_str());
-    });
+            JsonDocument doc;
+        });
 
     if (mqttClient.connect("48166572-b6cc-4618-b5fd-fb0ea59f595d")) {
         mqttClient.subscribe(SENSOR_TOPIC);
@@ -46,10 +66,11 @@ void setup() {
     }
 }
 
-
 void loop() {
     ensureWifi();
     ensureMQTT();
+
+    updateBlink();
 }
 
 
@@ -83,5 +104,75 @@ static void ensureMQTT() {
             mqttClient.subscribe(SENSOR_TOPIC);
             mqttClient.subscribe(COMMANDS_TOPIC);
         }
+    }
+}
+
+void execCommand(const byte *payload, const unsigned int length) {
+    constexpr size_t MAX_COMMAND_LENGTH = 64;
+
+    if (length >= MAX_COMMAND_LENGTH) {
+        Serial.println("Command too long");
+        return;
+    }
+
+    char message[MAX_COMMAND_LENGTH];
+
+    memcpy(message, payload, length);
+    message[length] = '\0';
+
+    if (strcmp(message, "manual_read") == 0) {
+        Serial.println("Manual trigger received.");
+
+        blinking = true;
+        blinkState = true;
+        blinkTransitions = 0;
+        lastBlinkTs = millis();
+
+        digitalWrite(LED_PIN, HIGH);
+    }
+}
+
+void updateSensors(byte *payload, unsigned int length) {
+    JsonDocument doc;
+
+    const DeserializationError error = deserializeJson(doc, payload, length);
+
+    if (error) {
+        Serial.printf("JSON error: %s\n", error.c_str());
+        return;
+    }
+
+    const float temperature = doc["temperature"];
+    const float humidity = doc["humidity"];
+
+    if (temperature > 26) {
+        // enable led
+    } else if (temperature < 20) {
+        // disable led
+    }
+
+    Serial.printf("%.1f %.1f\r\n", temperature, humidity);
+}
+
+void updateBlink() {
+    if (!blinking) {
+        return;
+    }
+
+    if (millis() - lastBlinkTs < BLINK_INTERVAL_MS) {
+        return;
+    }
+
+    lastBlinkTs = millis();
+
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState ? HIGH : LOW);
+
+    ++blinkTransitions;
+
+    if (blinkTransitions >= BLINK_COUNT * 2) {
+        blinking = false;
+        blinkState = false;
+        digitalWrite(LED_PIN, LOW);
     }
 }
