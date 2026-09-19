2fe6a71a110aba95d72b1ce153df908c6a029675
poiuntwl <vovanchik999999999@gmail.com>
2026-09-20 00:41:31 +0300

    main fill with placeholder data, export dashboard

--- hw6/fast_api/main.py
+++ hw6/fast_api/main.py
@@ -3,6 +3,7 @@ import os
 from fastapi import FastAPI
 import boto3
 import json
+from datetime import datetime, timezone, timedelta
 
 os.environ["AWS_PROFILE"] = "fastapi-backend"
 
@@ -11,12 +12,44 @@ iot = boto3.client("iot-data")
 
 @app.get("/sensors/latest")
 async def get_sensors_latest():
-    return {"Hello": "World"}
+    return {
+        "timestamp": str(datetime.now(timezone.utc)),
+        "humidity": 73
+    }
 
 
-@app.get("/sensors/history?minutes={mins}")
-async def get_sensors_history(mins: int = 30):
-    return {"mins": mins}
+@app.get("/sensors/history")
+async def get_sensors_history(minutes: int = 30):
+    return [
+    {
+        "timestamp": datetime.now(timezone.utc),
+        "temperature": 22.8
+    },
+    {
+        "timestamp": datetime.now(timezone.utc) - timedelta(minutes=5),
+        "temperature": 22.6
+    },
+    {
+        "timestamp": datetime.now(timezone.utc) - timedelta(minutes=10),
+        "temperature": 22.3
+    },
+    {
+        "timestamp": datetime.now(timezone.utc) - timedelta(minutes=15),
+        "temperature": 22.1
+    },
+    {
+        "timestamp": datetime.now(timezone.utc) - timedelta(minutes=20),
+        "temperature": 21.9
+    },
+    {
+        "timestamp": datetime.now(timezone.utc) - timedelta(minutes=25),
+        "temperature": 21.7
+    },
+    {
+        "timestamp": datetime.now(timezone.utc) - timedelta(minutes=30),
+        "temperature": 21.5
+    }
+]
 
 
 @app.post("/actuators/led")
