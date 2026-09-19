0970e5c52ea6479ea807b9cd90fb623a9b50bbd8
poiuntwl <vovanchik999999999@gmail.com>
2026-09-20 00:04:09 +0300

    add actual endpoints and make publishing work

--- hw6/fast_api/main.py
+++ hw6/fast_api/main.py
@@ -1,20 +1,31 @@
+import os
+
 from fastapi import FastAPI
+import boto3
+import json
+
+os.environ["AWS_PROFILE"] = "fastapi-backend"
 
 app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+iot = boto3.client("iot-data")
 
 @app.get("/sensors/latest")
 async def get_sensors_latest():
     return {"Hello": "World"}
 
+
 @app.get("/sensors/history?minutes={mins}")
 async def get_sensors_history(mins: int = 30):
     return {"mins": mins}
 
+
 @app.post("/actuators/led")
 async def post_actuators_led():
+    iot.publish(
+        topic="iot-course/volodya/actuators/led",
+        qos=1,
+        payload=json.dumps({
+            "state": "on"
+        })
+    )
     return {"Hello": "World"}
